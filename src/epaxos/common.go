b2e88d0aa17190250475c7b76fb839819ac334bd
Katie <katieliu@mit.edu>
2024-04-27 21:05:19 -0400

    added structs & removed raft stuff

--- src/epaxos/common.go
+++ src/epaxos/common.go
@@ -10,8 +10,8 @@ type Instance struct {
 	Command  interface{} // state machine command
 	Position LogIndex
 }
-type Status int
 
+type Status int
 // Enum values
 const (
 	PREACCEPTED Status = iota
@@ -19,3 +19,56 @@ const (
 	COMMITTED
 	EXECUTED
 )
+
+// Pre-Accept RPC arguments structure
+type PreAcceptArgs struct {
+	Command 	interface{} // command
+	Deps 		[]Instance 	// list of all instances that contain commands that interfere with this command
+	Seq 		int 		// sequence number used to break dependencies
+
+	Ballot 		int 		// ballot number
+	Replica 	int 		// replica number
+	Instance 	int 		// instance number of leader replica
+	Leader 		int 		// command leader
+}
+
+// Pre-Accept RPC reply structure
+type PreAcceptReply struct {
+	Deps 	[]Instance 	// updated list of dependencies
+	Seq 	int 		// updated sequence number
+}
+
+// Accept RPC arguments structure
+type AcceptArgs struct {
+	Command 	interface{} // command
+	Deps 		[]Instance 	// list of all instances that contain commands that interfere with this command
+	Seq 		int 		// sequence number used to break dependencies
+
+	Ballot 		int 		// ballot number
+	Replica 	int 		// replica number
+	Instance 	int 		// instance number of leader replica
+	Leader 		int 		// command leader
+}
+
+// Accept RPC reply structure
+type AcceptReply struct {
+	Success bool // true if replica accepted command
+
+}
+
+// Commit RPC arguments structure
+type CommitArgs struct {
+	Command 	interface{} // command
+	Deps 		[]Instance 	// list of all instances that contain commands that interfere with this command
+	Seq 		int 		// sequence number used to break dependencies
+
+	Ballot 		int 		// ballot number
+	Replica 	int 		// replica number
+	Instance 	int 		// instance number of leader replica
+	Leader 		int 		// command leader
+}
+
+// Commit RPC reply structure
+type CommitReply struct {
+	Success bool // true if replica committed command
+}
