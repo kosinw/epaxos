7c899f7d33152ac3453b4bf78e451ac9a71c7276
Kosi Nwabueze <kosinw@mit.edu>
2024-05-06 21:41:34 -0400

    started working on epaxoskv

---
+++ src/epaxoskv/common.go
@@ -0,0 +1,40 @@
+package epaxoskv
+
+const (
+	OK                = "OK"
+	ErrNoKey          = "no key found"
+	ErrNotCommitted   = "command could not be committed"
+	ErrNetworkTimeout = "network timeout"
+)
+
+type Err string
+
+const (
+	OpGet    = "GET"
+	OpPut    = "PUT"
+	OpAppend = "APPEND"
+)
+
+// Put or Append
+type PutAppendArgs struct {
+	Key     string
+	Value   string
+	Op      string
+	ClerkId string // unique ID for clerk
+	SeqNum  int    // monotonically increasing sequence number
+}
+
+type PutAppendReply struct {
+	Err Err
+}
+
+type GetArgs struct {
+	Key     string
+	ClerkId string // unique ID for clerk
+	SeqNum  int    // monotonically increasing sequence number
+}
+
+type GetReply struct {
+	Err   Err
+	Value string
+}
---
+++ src/epaxoskv/test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+rm -rf 2024*
+
+../../scripts/dstest -v -v --workers 3 --iter $1 --race -t \
+    TestBasic4A \
+    TestSpeed4A \
+    TestConcurrent4A \
+    TestUnreliable4A \
+    TestUnreliableOneKey4A \
+    TestOnePartition4A \
+    TestManyPartitionsOneClient4A \
+    TestManyPartitionsManyClients4A \
+    TestPersistOneClient4A \
+    TestPersistConcurrent4A \
+    TestPersistConcurrentUnreliable4A \
+    TestPersistPartition4A \
+    TestPersistPartitionUnreliable4A \
+    TestPersistPartitionUnreliableLinearizable4A \
